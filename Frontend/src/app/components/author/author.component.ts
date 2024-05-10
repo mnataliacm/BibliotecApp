05d18cf5a9e410e61f91cde05c62fea7fcdb1899
Natalia Castillo Muñoz <n.castillo@accenture.com>
2024-05-10 12:32:23 +0200

    componentes en usuario, libro, categoria, autor pendiente aplicar formato y funcionalidad

--- Frontend/src/app/components/author/author.component.ts
+++ Frontend/src/app/components/author/author.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AuthorComponent {
 
+//   accordion.toggle('first'); // toggle the first item
+// accordion.closeAll(); 		 // close all items
+
+// first.toggle();  				// toggle the first item
+// first.collapsed; 				// true if collapsed
+// first.collapsed = true; // collapse the first item
+// first.disabled; 			 	// true if disabled
+// first.disabled = true; 	// disable the first item
+// first.id;        				// 'first'
+
 }
